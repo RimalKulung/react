07be70c9b47a09be9c6c97d944615081c16124c3
Rimal Kulung <rimalklg@gmail.com>
2024-01-10 16:36:53 +0545

    theme switcher

---
+++ 09themeswitcher/src/context/Theme.js
@@ -0,0 +1,19 @@
+import { useDeferredValue } from "react";
+import { createContext, useContext } from "react";
+
+
+
+export const ThemeContext= createContext({
+    themeMode: 'light',
+    darkTheme: () =>{},
+    lightTheme: () =>{},
+
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function(){
+    return(
+        useContext(ThemeContext)
+    )
+}
